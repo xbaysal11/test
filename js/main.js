34427f24e75fde775d7318a8d46d7279584235f5
xbaysal11 <baisalbek.daniiarov@gmail.com>
2020-02-03 12:23:05 +0600

    ADD test

---
+++ js/main.js
@@ -0,0 +1,137 @@
+/*
+ *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+/* global AudioContext, SoundMeter */
+
+'use strict';
+
+const instantMeter = document.querySelector('#instant meter');
+const slowMeter = document.querySelector('#slow meter');
+const clipMeter = document.querySelector('#clip meter');
+
+const instantValueDisplay = document.querySelector('#instant .value');
+const slowValueDisplay = document.querySelector('#slow .value');
+const clipValueDisplay = document.querySelector('#clip .value');
+
+try {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    window.audioContext = new AudioContext();
+} catch (e) {
+    alert('Web Audio API not supported.');
+}
+
+// Put variables in global scope to make them available to the browser console.
+const constraints = (window.constraints = {
+  audio: true,
+  video: false
+});
+
+function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      alert('IOS');
+
+        function handleSuccess(stream) {
+            window.stream = stream;
+            const soundMeter = (window.soundMeter = new SoundMeter(
+                window.audioContext
+            ));
+            soundMeter.connectToSource(stream, function(e) {
+                // if (e) {
+                //     alert(e);
+                //     return;
+                // }
+                // setInterval(() => {
+                    // instantMeter.value = instantValueDisplay.innerText = soundMeter.instant.toFixed(
+                    //     2
+                    // );
+
+                    // slowMeter.value = slowValueDisplay.innerText = soundMeter.slow.toFixed(
+                    //     2
+                    // );
+                    // clipMeter.value = clipValueDisplay.innerText = soundMeter.clip;
+                // }, 200);
+                if (soundMeter.instant.toFixed(2) > 0.40) {
+                  body.querySelector('.flame').style.display = 'none';
+                    setTimeout(() => {
+                        body.querySelector('.flame').style.display = '';
+                        console.log('FIRE!');
+                    }, 5000);
+                }
+            });
+        }
+
+        function handleError(error) {
+            console.log(
+                'navigator.MediaDevices.getUserMedia error: ',
+                error.message,
+                error.name
+            );
+        }
+
+        navigator.mediaDevices
+            .getUserMedia(constraints)
+            .then(handleSuccess)
+            .catch(handleError);
+    } else {
+        var body,
+            num,
+            array,
+            width,
+            context,
+            analyser,
+            src,
+            height;
+
+        body = document.querySelector('body');
+        num = 32;
+        array = new Uint8Array(num * 2);
+        width = 10;
+        window.onclick = function() {
+            if (context) return;
+            context = new AudioContext();
+            analyser = context.createAnalyser();
+            context.resume().then(() => {
+                console.log('Playback resumed successfully');
+            });
+            navigator.mediaDevices
+                .getUserMedia({
+                    audio: true
+                })
+                .then(stream => {
+                    src = context.createMediaStreamSource(stream);
+                    src.connect(analyser);
+                    loop();
+                })
+                .catch(error => {
+                    alert(error + '\r\n Отклонено. Страница будет обновлена!');
+                    location.reload();
+                });
+        };
+
+        function loop() {
+            window.requestAnimationFrame(loop);
+            analyser.getByteFrequencyData(array);
+            for (var i = 0; i < num; i++) {
+                height = array[i + num];
+                if (height > 210) {
+                    console.log('OFF!');
+                    console.log(`Volume: ${height}`);
+
+                    body.querySelector('.flame').style.display = 'none';
+                    setTimeout(() => {
+                        body.querySelector('.flame').style.display = '';
+                        console.log('FIRE!');
+                    }, 5000);
+                }
+            }
+        }
+        alert('Click candle once to start!');
+    }
+}
+getMobileOperatingSystem();
