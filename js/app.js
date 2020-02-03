1e0c7eca2fc56bf2b028218115eb8398d7e59c33
xbaysal11 <baisalbek.daniiarov@gmail.com>
2020-02-03 15:00:01 +0600

    ADD test

--- js/app.js
+++ js/app.js
@@ -1,51 +1,58 @@
 function getMobileOperatingSystem() {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+       
+        console.log('ios');
+
         window.onload = function() {
-            const constraints = (window.constraints = {
+
+            try {
+                window.AudioContext = window.AudioContext || window.webkitAudioContext;
+                window.audioContext = new AudioContext();
+              } catch (e) {
+                alert('Web Audio API not supported.');
+              }
+              
+              // Put variables in global scope to make them available to the browser console.
+              const constraints = window.constraints = {
                 audio: true,
                 video: false
-            });
-            function handleSuccess(stream) {
+              };
+              
+              function handleSuccess(stream) {
+                // Put variables in global scope to make them available to the
+                // browser console.
                 window.stream = stream;
-                const soundMeter = (window.soundMeter = new SoundMeter(
-                    window.audioContext
-                ));
+                const soundMeter = window.soundMeter = new SoundMeter(window.audioContext);
                 soundMeter.connectToSource(stream, function(e) {
-                    if (e) {
-                        alert(e);
-                        return;
+                  if (e) {
+                    alert(e);
+                    return;
+                  }
+                  setInterval(() => {
+                    console.log(soundMeter.instant.toFixed(2));
+                    if (soundMeter.instant.toFixed(2) >= 0.1) {
+                        alert(soundMeter.instant.toFixed(2));
+                        var body = document.querySelector('body');
+                        body.querySelector('.flame').style.display = 'none';
+                        setTimeout(() => {
+                            body.querySelector('.flame').style.display = '';
+                            console.log('FIRE!');
+                        }, 5000);
                     }
-                    setInterval(() => {
-                        console.log(soundMeter.instant.toFixed(2));
-                        if (soundMeter.instant.toFixed(2) >= 0.1) {
-                            alert(soundMeter.instant.toFixed(2));
-                            var body = document.querySelector('body');
-                            body.querySelector('.flame').style.display = 'none';
-                            setTimeout(() => {
-                                body.querySelector('.flame').style.display = '';
-                                console.log('FIRE!');
-                            }, 5000);
-                        }
-                    }, 200);
+                  }, 200);
                 });
-            }
-        
-            function handleError(error) {
-                console.log(
-                    'navigator.MediaDevices.getUserMedia error: ',
-                    error.message,
-                    error.name
-                );
-            }
-        
-            navigator.mediaDevices
-                .getUserMedia(constraints)
-                .then(handleSuccess)
-                .catch(handleError);
-        };
-        
+              }
+              
+              function handleError(error) {
+                console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+              }
+              
+              navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+            };
+            
     } else {
+        console.log('android');
         var body, num, array, width, context, logo, myElements, analyser, src, height;
 
         body = document.querySelector('body');
@@ -100,7 +107,4 @@ function getMobileOperatingSystem() {
         
     }
 }
-window.onload = function() {
     getMobileOperatingSystem();
-
-}
